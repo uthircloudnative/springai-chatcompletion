ed5a13132f8aa82a0ebcbd081c1385abce3966fc
uthircloudnative <uthircloudnative@gmail.com>
2025-07-05 00:07:29 -0400

    Streaming Output Impl

--- src/main/java/com/springai/chatcompletion/controller/VertexAIChatController.java
+++ src/main/java/com/springai/chatcompletion/controller/VertexAIChatController.java
@@ -1,10 +1,12 @@
 package com.springai.chatcompletion.controller;
 
 import com.springai.chatcompletion.service.VertexAIChatService;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import reactor.core.publisher.Flux;
 
 import java.util.Map;
 
@@ -26,5 +28,10 @@ public class VertexAIChatController {
     @GetMapping(value="/vertexai/chat")
     public Map<String,String> chat(@RequestParam(value="prompt") String prompt) {
         return vertexAIChatService.chat(prompt);
+    }
+
+    @GetMapping(value="/vertexai/chat/stream", produces = MediaType.APPLICATION_NDJSON_VALUE)
+    public Flux<String> chatStream(@RequestParam(value="prompt") String prompt ){
+        return vertexAIChatService.chatStream(prompt);
     }
 }
--- src/main/java/com/springai/chatcompletion/service/VertexAIChatService.java
+++ src/main/java/com/springai/chatcompletion/service/VertexAIChatService.java
@@ -1,7 +1,10 @@
 package com.springai.chatcompletion.service;
 
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.vertexai.gemini.VertexAiGeminiChatModel;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 
 import java.util.Map;
 
@@ -15,6 +18,7 @@ public class VertexAIChatService {
 
     private final VertexAiGeminiChatModel vertexAiGeminiChatModel;
 
+
     public VertexAIChatService(VertexAiGeminiChatModel vertexAiGeminiChatModel) {
         this.vertexAiGeminiChatModel = vertexAiGeminiChatModel;
     }
@@ -22,5 +26,18 @@ public class VertexAIChatService {
     public Map<String, String> chat(String prompt) {
         return Map.of("message", vertexAiGeminiChatModel.call(prompt));
     }
+
+    public Flux<String> chatStream(String prompt) {
+
+        UserMessage userMessage = new UserMessage(prompt);
+        Prompt userPrompt = Prompt.builder()
+                                .messages(userMessage)
+                                .build();
+
+        return vertexAiGeminiChatModel.stream(userPrompt)
+                                      .map(chatresponse -> chatresponse.getResult().getOutput().getText());
+    }
+
+
 
 }
